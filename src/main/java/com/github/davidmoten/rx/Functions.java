2f8c346f68cff49d092f4f6183e0b60091853ee4
Dave Moten <davidmoten@gmail.com>
2014-11-29 10:39:58 +1100

    add code

---
+++ src/main/java/com/github/davidmoten/rx/Functions.java
@@ -0,0 +1,35 @@
+package com.github.davidmoten.rx;
+
+import rx.functions.Func1;
+
+public class Functions {
+
+    public static <T> Func1<T, T> identity() {
+        return new Func1<T, T>() {
+
+            @Override
+            public T call(T t) {
+                return t;
+            }
+        };
+    }
+
+    public static <T> Func1<T, Boolean> alwaysTrue() {
+        return new Func1<T, Boolean>() {
+            @Override
+            public Boolean call(T t) {
+                return true;
+            }
+        };
+    }
+
+    public static <T> Func1<T, Boolean> alwaysFalse() {
+        return new Func1<T, Boolean>() {
+            @Override
+            public Boolean call(T t) {
+                return false;
+            }
+        };
+    }
+
+}
---
+++ src/test/java/com/github/davidmoten/rx/FunctionsTest.java
@@ -0,0 +1,25 @@
+package com.github.davidmoten.rx;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FunctionsTest {
+
+    @Test
+    public void testIdentity() {
+        assertEquals(123, (int) Functions.<Integer> identity().call(123));
+    }
+
+    @Test
+    public void testAlwaysTrue() {
+        assertTrue(Functions.<Integer> alwaysTrue().call(123));
+    }
+
+    @Test
+    public void testAlwaysFalse() {
+        assertFalse(Functions.<Integer> alwaysFalse().call(123));
+    }
+}
