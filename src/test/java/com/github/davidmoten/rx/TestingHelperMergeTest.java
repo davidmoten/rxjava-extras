f381cced7ded1d8c86a1c7998fcf1aed2205ca8d
Dave Moten <davidmoten@gmail.com>
2014-12-27 20:36:15 +1100

    change test to work on Observable<Integer> as simplification

--- src/test/java/com/github/davidmoten/rx/TestingHelperMergeTest.java
+++ src/test/java/com/github/davidmoten/rx/TestingHelperMergeTest.java
@@ -10,25 +10,24 @@ import com.github.davidmoten.rx.testing.TestingHelper;
 
 public class TestingHelperMergeTest extends TestCase {
 
-    private static final Func1<Observable<String>, Observable<String>> onBufferBackpressure = new Func1<Observable<String>, Observable<String>>() {
+    private static final Func1<Observable<Integer>, Observable<Integer>> merge = new Func1<Observable<Integer>, Observable<Integer>>() {
         @Override
-        public Observable<String> call(Observable<String> o) {
-            return o.mergeWith(Observable.from(asList("x", "y", "z")));
+        public Observable<Integer> call(Observable<Integer> o) {
+            return o.mergeWith(Observable.from(asList(7, 8, 9)));
         }
     };
 
     public static TestSuite suite() {
 
-        return TestingHelper.function(onBufferBackpressure)
-                // test empty
-                .name("testEmpty").fromEmpty().expect("x", "y", "z")
+        return TestingHelper.function(merge)
+        // test empty
+                .name("testEmpty").fromEmpty().expect(7, 8, 9)
                 // test non-empty count
-                .name("testTwo").from("a", "b").expectAnyOrder("x", "y", "z", "a", "b")
+                .name("testTwo").from(1, 2).expectAnyOrder(1, 7, 8, 9, 2)
                 // test single input
-                .name("testOne").from("a").expectAnyOrder("x", "y", "z", "a")
+                .name("testOne").from(1).expectAnyOrder(7, 1, 8, 9)
                 // unsub before completion
-                .name("testSomeUnsubscribeAfterOne").from("a", "b").unsubscribeAfter(1)
-                .expectSize(1)
+                .name("testSomeUnsubscribeAfterOne").from(1, 2).unsubscribeAfter(1).expectSize(1)
                 // get test suites
                 .testSuite(TestingHelperMergeTest.class);
     }
