998e50db27f7826c179e988f7a1f170fc214a6e2
Dave Moten <davidmoten@gmail.com>
2015-01-27 14:21:13 +1100

    unsubcribe eagerly on error too

--- src/main/java/com/github/davidmoten/rx/operators/OperatorUnsubscribeEagerly.java
+++ src/main/java/com/github/davidmoten/rx/operators/OperatorUnsubscribeEagerly.java
@@ -5,42 +5,43 @@ import rx.Subscriber;
 
 public final class OperatorUnsubscribeEagerly<T> implements Operator<T, T> {
 
-	private OperatorUnsubscribeEagerly() {
-		// no instantiation outside of this class
-	}
+    private OperatorUnsubscribeEagerly() {
+        // no instantiation outside of this class
+    }
 
-	private static final class Singleton {
-		private static final OperatorUnsubscribeEagerly<?> INSTANCE = new OperatorUnsubscribeEagerly<Object>();
-	}
+    private static final class Singleton {
+        private static final OperatorUnsubscribeEagerly<?> INSTANCE = new OperatorUnsubscribeEagerly<Object>();
+    }
 
-	@SuppressWarnings("unchecked")
-	public static final <T> OperatorUnsubscribeEagerly<T> instance() {
-		return (OperatorUnsubscribeEagerly<T>) Singleton.INSTANCE;
-	}
+    @SuppressWarnings("unchecked")
+    public static final <T> OperatorUnsubscribeEagerly<T> instance() {
+        return (OperatorUnsubscribeEagerly<T>) Singleton.INSTANCE;
+    }
 
-	@Override
-	public Subscriber<? super T> call(final Subscriber<? super T> child) {
-		Subscriber<T> parent = new Subscriber<T>() {
+    @Override
+    public Subscriber<? super T> call(final Subscriber<? super T> child) {
+        Subscriber<T> parent = new Subscriber<T>() {
 
-			@Override
-			public void onCompleted() {
-				unsubscribe();
-				child.onCompleted();
-			}
+            @Override
+            public void onCompleted() {
+                unsubscribe();
+                child.onCompleted();
+            }
 
-			@Override
-			public void onError(Throwable e) {
-				child.onError(e);
-			}
+            @Override
+            public void onError(Throwable e) {
+                unsubscribe();
+                child.onError(e);
+            }
 
-			@Override
-			public void onNext(T t) {
-				child.onNext(t);
-			}
+            @Override
+            public void onNext(T t) {
+                child.onNext(t);
+            }
 
-		};
-		child.add(parent);
-		return parent;
-	}
+        };
+        child.add(parent);
+        return parent;
+    }
 
 }
