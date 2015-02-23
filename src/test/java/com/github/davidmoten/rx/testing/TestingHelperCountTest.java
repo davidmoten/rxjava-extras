893fa291d2cd1f5502a42dfcfbc90a068bedde43
Dave Moten <davidmoten@gmail.com>
2015-02-24 08:41:26 +1100

    upgrade to rxjava 1.0.7, iterable backp now available so can now get rid of global flag on TestingHelper to stop request overflow testing

--- src/test/java/com/github/davidmoten/rx/testing/TestingHelperCountTest.java
+++ src/test/java/com/github/davidmoten/rx/testing/TestingHelperCountTest.java
@@ -7,39 +7,38 @@ import rx.functions.Func1;
 
 public class TestingHelperCountTest extends TestCase {
 
-	public static TestSuite suite() {
-		TestingHelper.includeBackpressureRequestOverflowTest = false;
-		return TestingHelper
-				.function(COUNT)
-				// test empty
-				.name("testCountOfEmptyReturnsZero").fromEmpty()
-				.expect(0)
-				// test error
-				.name("testCountErrorReturnsError").fromError()
-				.expectError()
-				// test error after some emission
-				.name("testCountErrorAfterTwoEmissionsReturnsError")
-				.fromErrorAfter(5, 6).expectError()
-				// test non-empty count
-				.name("testCountOfTwoReturnsTwo").from(5, 6).expect(2)
-				// test single input
-				.name("testCountOfOneReturnsOne").from(5).expect(1)
-				// count many
-				.name("testCountOfManyDoesNotGiveStackOverflow")
-				.from(Observable.range(1, 1000000)).expect(1000000)
-				// get test suites
-				.testSuite(TestingHelperCountTest.class);
-	}
+    public static TestSuite suite() {
+        return TestingHelper
+                .function(COUNT)
+                // test empty
+                .name("testCountOfEmptyReturnsZero").fromEmpty()
+                .expect(0)
+                // test error
+                .name("testCountErrorReturnsError").fromError()
+                .expectError()
+                // test error after some emission
+                .name("testCountErrorAfterTwoEmissionsReturnsError").fromErrorAfter(5, 6)
+                .expectError()
+                // test non-empty count
+                .name("testCountOfTwoReturnsTwo").from(5, 6).expect(2)
+                // test single input
+                .name("testCountOfOneReturnsOne").from(5).expect(1)
+                // count many
+                .name("testCountOfManyDoesNotGiveStackOverflow").from(Observable.range(1, 1000000))
+                .expect(1000000)
+                // get test suites
+                .testSuite(TestingHelperCountTest.class);
+    }
 
-	public void testDummy() {
-		// just here to fool eclipse
-	}
+    public void testDummy() {
+        // just here to fool eclipse
+    }
 
-	private static final Func1<Observable<Integer>, Observable<Integer>> COUNT = new Func1<Observable<Integer>, Observable<Integer>>() {
-		@Override
-		public Observable<Integer> call(Observable<Integer> o) {
-			return o.count();
-		}
-	};
+    private static final Func1<Observable<Integer>, Observable<Integer>> COUNT = new Func1<Observable<Integer>, Observable<Integer>>() {
+        @Override
+        public Observable<Integer> call(Observable<Integer> o) {
+            return o.count();
+        }
+    };
 
 }
