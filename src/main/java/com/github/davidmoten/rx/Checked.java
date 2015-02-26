eb9ecb91ecaed94616d4bcf7d29b21b9f7764bd5
Dave Moten <davidmoten@gmail.com>
2015-02-27 10:41:12 +1100

    add Checked

--- src/main/java/com/github/davidmoten/rx/Checked.java
+++ src/main/java/com/github/davidmoten/rx/Checked.java
@@ -6,31 +6,46 @@ import rx.functions.Func0;
 import rx.functions.Func1;
 import rx.functions.Func2;
 
+/**
+ * Utility functions that are useful for brevity when using checked exceptions
+ * in lambdas with RxJava.
+ */
 public final class Checked {
 
     public static interface F0<T> {
         T call() throws Exception;
     }
-    public static interface F1<T,R> {
+
+    public static interface F1<T, R> {
         R call(T t) throws Exception;
     }
-    
-    public static interface F2<T,R,S> {
-        S call(T t,R r) throws Exception;
+
+    public static interface F2<T, R, S> {
+        S call(T t, R r) throws Exception;
     }
-    
-    
+
     public static interface A0 {
         void call() throws Exception;
     }
-    
+
     public static interface A1<T> {
         void call(T t) throws Exception;
     }
-    
-    public static <T> Func0<T> f0(final F0<T> f) {
-        return new Func0<T>(){
 
+    /**
+     * Returns a {@link Func0} that reports any exception thrown by f wrapped by
+     * a {@link RuntimeException}.
+     * 
+     * @param f
+     *            has same signature as Func0 but can throw an exception
+     * @param <T>
+     *            type parameter
+     * @return a {@link Func0} that reports any exception thrown by f wrapped by
+     *         a {@link RuntimeException}.
+     * 
+     */
+    public static <T> Func0<T> f0(final F0<T> f) {
+        return new Func0<T>() {
             @Override
             public T call() {
                 try {
@@ -39,13 +54,12 @@ public final class Checked {
                     throw new RuntimeException(e);
                 }
             }
-            
+
         };
     }
-    
-    public static <T,R> Func1<T,R> f1(final F1<T,R> f) {
-        return new Func1<T,R>(){
 
+    public static <T, R> Func1<T, R> f1(final F1<T, R> f) {
+        return new Func1<T, R>() {
             @Override
             public R call(T t) {
                 try {
@@ -54,28 +68,26 @@ public final class Checked {
                     throw new RuntimeException(e);
                 }
             }
-            
+
         };
     }
-    
-    public static <T,R,S> Func2<T,R,S> f2(final F2<T,R,S> f) {
-        return new Func2<T,R,S>(){
 
+    public static <T, R, S> Func2<T, R, S> f2(final F2<T, R, S> f) {
+        return new Func2<T, R, S>() {
             @Override
-            public S call(T t,R r) {
+            public S call(T t, R r) {
                 try {
-                    return f.call(t,r);
+                    return f.call(t, r);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             }
-            
+
         };
     }
-    
-    public static Action0 a0(final A0 a) {
-        return new Action0(){
 
+    public static Action0 a0(final A0 a) {
+        return new Action0() {
             @Override
             public void call() {
                 try {
@@ -86,10 +98,9 @@ public final class Checked {
             }
         };
     }
-    
-    public static <T> Action1<T> a1(final A1<T> a) {
-        return new Action1<T>(){
 
+    public static <T> Action1<T> a1(final A1<T> a) {
+        return new Action1<T>() {
             @Override
             public void call(T t) {
                 try {
@@ -100,6 +111,5 @@ public final class Checked {
             }
         };
     }
-    
-    
+
 }
