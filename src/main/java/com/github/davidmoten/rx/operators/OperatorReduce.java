74e0eccdcb9bc6d658c48fa10d9e65353b0e1a1b
Dave Moten <davidmoten@gmail.com>
2015-04-15 21:28:11 +1000

    add OperatorReduce

---
+++ src/main/java/com/github/davidmoten/rx/operators/OperatorReduce.java
@@ -0,0 +1,104 @@
+package com.github.davidmoten.rx.operators;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import rx.Observable.Operator;
+import rx.Producer;
+import rx.Subscriber;
+import rx.functions.Func2;
+
+public class OperatorReduce<T, R> implements Operator<R, T> {
+
+	private final Func2<R, ? super T, R> reduction;
+	private R initialValue;
+
+	public OperatorReduce(Func2<R, ? super T, R> reduction, R initialValue) {
+		this.reduction = reduction;
+		this.initialValue = initialValue;
+	}
+
+	@Override
+	public Subscriber<? super T> call(Subscriber<? super R> child) {
+		final ParentSubscriber<T, R> parent = new ParentSubscriber<T, R>(child,
+				reduction, initialValue);
+		child.setProducer(new Producer() {
+
+			@Override
+			public void request(long n) {
+				parent.requestMore(n);
+			}
+		});
+		child.add(parent);
+		return parent;
+	}
+
+	private static class ParentSubscriber<T, R> extends Subscriber<T> {
+
+		private static enum State {
+			NOT_REQUESTED_NOT_COMPLETED, NOT_REQUESTED_COMPLETED, REQUESTED_NOT_COMPLETED, REQUESTED_COMPLETED, EMITTED;
+		}
+
+		private final Subscriber<? super R> child;
+		private R value;
+		private AtomicReference<State> state = new AtomicReference<State>(
+				State.NOT_REQUESTED_NOT_COMPLETED);
+		private final Func2<R, ? super T, R> reduction;
+
+		ParentSubscriber(Subscriber<? super R> child,
+				Func2<R, ? super T, R> reduction, R initialValue) {
+			this.child = child;
+			this.reduction = reduction;
+			this.value = initialValue;
+		}
+
+		void requestMore(long n) {
+			if (n > 0) {
+				if (!state.compareAndSet(State.NOT_REQUESTED_NOT_COMPLETED,
+						State.REQUESTED_NOT_COMPLETED)) {
+					if (state.compareAndSet(State.NOT_REQUESTED_COMPLETED,
+							State.REQUESTED_COMPLETED)) {
+						drain();
+					}
+				}
+			}
+		}
+
+		@Override
+		public void onCompleted() {
+			if (state.compareAndSet(State.REQUESTED_NOT_COMPLETED,
+					State.REQUESTED_COMPLETED)) {
+				drain();
+			} else {
+				state.compareAndSet(State.NOT_REQUESTED_NOT_COMPLETED,
+						State.NOT_REQUESTED_COMPLETED);
+			}
+		}
+
+		void drain() {
+			if (state.compareAndSet(State.REQUESTED_COMPLETED, State.EMITTED)) {
+				if (isUnsubscribed())
+					return;
+				// synchronize to ensure that value is safely published
+				synchronized (this) {
+					child.onNext(value);
+					// release for gc
+					value = null;
+					if (!isUnsubscribed())
+						child.onCompleted();
+				}
+			}
+		}
+
+		@Override
+		public void onError(Throwable e) {
+			child.onError(e);
+		}
+
+		@Override
+		public void onNext(T t) {
+			value = reduction.call(value, t);
+		}
+
+	}
+
+}
