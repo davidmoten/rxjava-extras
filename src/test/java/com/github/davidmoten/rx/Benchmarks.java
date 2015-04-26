0c609dc3fd9185e9a131ef9bb5680796b8b095ab
Dave Moten <davidmoten@gmail.com>
2015-04-26 21:00:33 +1000

    move OperatorTakeLastOne to rxjava library as a PR

--- src/test/java/com/github/davidmoten/rx/Benchmarks.java
+++ src/test/java/com/github/davidmoten/rx/Benchmarks.java
@@ -4,42 +4,13 @@ import org.openjdk.jmh.annotations.Benchmark;
 
 import rx.Observable;
 
-import com.github.davidmoten.rx.operators.OperatorTakeLastOne;
-
 public class Benchmarks {
 
     private static final int MANY = 1000000;
-    private static final int SOME = 100;
-    private static final int FEW = 5;
 
     @Benchmark
     public void takeLastOneFromRxJavaLibraryMany() {
         Observable.range(1, MANY).takeLast(1).subscribe();
-    }
-
-    @Benchmark
-    public void takeLastOneFromRxJavaLibraryFew() {
-        Observable.range(1, FEW).takeLast(1).subscribe();
-    }
-
-    @Benchmark
-    public void takeLastOneFromRxJavaLibrarySome() {
-        Observable.range(1, SOME).takeLast(1).subscribe();
-    }
-
-    @Benchmark
-    public void takeLastOneNewMany() {
-        Observable.range(1, MANY).lift(OperatorTakeLastOne.<Integer> create()).subscribe();
-    }
-
-    @Benchmark
-    public void takeLastOneNewFew() {
-        Observable.range(1, FEW).lift(OperatorTakeLastOne.<Integer> create()).subscribe();
-    }
-
-    @Benchmark
-    public void takeLastOneNewSome() {
-        Observable.range(1, SOME).lift(OperatorTakeLastOne.<Integer> create()).subscribe();
     }
 
 }
