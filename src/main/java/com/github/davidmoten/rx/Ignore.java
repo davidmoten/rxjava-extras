993502486431ba46babccbe7fbd7dbb199cff831
Dave Moten <davidmoten@gmail.com>
2015-05-05 16:55:51 +1000

    add Ignore function helpers

---
+++ src/main/java/com/github/davidmoten/rx/Ignore.java
@@ -0,0 +1,36 @@
+package com.github.davidmoten.rx;
+
+import rx.functions.Action0;
+import rx.functions.Action1;
+
+import com.github.davidmoten.rx.Checked.A0;
+import com.github.davidmoten.rx.Checked.A1;
+
+public class Ignore {
+
+    public static Action0 a0(final A0 a) {
+        return new Action0() {
+            @Override
+            public void call() {
+                try {
+                    a.call();
+                } catch (Exception e) {
+                    // ignore
+                }
+            }
+        };
+    }
+
+    public static <T> Action1<T> a1(final A1<T> a) {
+        return new Action1<T>() {
+            @Override
+            public void call(T t) {
+                try {
+                    a.call(t);
+                } catch (Exception e) {
+                    // ignore
+                }
+            }
+        };
+    }
+}
