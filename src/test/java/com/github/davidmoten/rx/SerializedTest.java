85b710e8872d61733242094cd0b8d99cd556bb49
Dave Moten <davidmoten@gmail.com>
2015-06-28 21:05:14 +1000

    add another test for Serialized utility class

--- src/test/java/com/github/davidmoten/rx/SerializedTest.java
+++ src/test/java/com/github/davidmoten/rx/SerializedTest.java
@@ -14,8 +14,8 @@ import rx.Observable;
 public class SerializedTest {
 
     @Test
-    public void testSerializeAndDeserialize() {
-        File file = new File("target/temp123");
+    public void testSerializeAndDeserializeOfNonEmptyStream() {
+        File file = new File("target/temp1");
         file.delete();
         Observable<Integer> source = Observable.just(1, 2, 3);
         Serialized.write(source, file, false).subscribe();
@@ -23,6 +23,17 @@ public class SerializedTest {
         assertTrue(file.length()>0);
         List<Integer> list = Serialized.<Integer> read(file).toList().toBlocking().single();
         assertEquals(Arrays.asList(1,2,3),list);
+    }
+    
+    @Test
+    public void testSerializeAndDeserializeOfEmptyStream() {
+        File file = new File("target/temp2");
+        file.delete();
+        Observable<Integer> source = Observable.empty();
+        Serialized.write(source, file, false).subscribe();
+        assertTrue(file.exists());
+        List<Integer> list = Serialized.<Integer> read(file).toList().toBlocking().single();
+        assertTrue(list.isEmpty());
     }
 
 }
