4c58e8e988607ec51aca8e1801d7c4f907c5e4dc
Dave Moten <davidmoten@gmail.com>
2015-07-28 21:17:15 +1000

    remove async bufferEmissions and delete Operator.* api

--- src/main/java/com/github/davidmoten/rx/Transformers.java
+++ src/main/java/com/github/davidmoten/rx/Transformers.java
@@ -12,7 +12,6 @@ import rx.Observable;
 import rx.Observable.Operator;
 import rx.Observable.Transformer;
 import rx.Observer;
-import rx.Scheduler;
 import rx.functions.Action2;
 import rx.functions.Func0;
 import rx.functions.Func1;
@@ -99,16 +98,6 @@ public final class Transformers {
 
     public static <T> Transformer<T, Indexed<T>> mapWithIndex() {
         return MapWithIndex.instance();
-    }
-
-    public static <T> Transformer<T, T> bufferEmissions(final Scheduler scheduler) {
-        return new Transformer<T, T>() {
-
-            @Override
-            public Observable<T> call(Observable<T> o) {
-                return o.lift(new OperatorBufferEmissions<T>(scheduler));
-            }
-        };
     }
 
     /**
@@ -249,7 +238,13 @@ public final class Transformers {
 
     // holder lazy singleton pattern
     private static class BufferEmissionsHolder {
-        static Transformer<Object, Object> INSTANCE = bufferEmissions(null);
+        static Transformer<Object, Object> INSTANCE = new Transformer<Object, Object>() {
+
+            @Override
+            public Observable<Object> call(Observable<Object> o) {
+                return o.lift(new OperatorBufferEmissions<Object>());
+            }
+        };
     }
 
     /**
