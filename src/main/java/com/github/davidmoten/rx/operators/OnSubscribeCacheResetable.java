cdf2eea8e96e4efc499f33701c5f097d9b8321c9
Dave Moten <davidmoten@gmail.com>
2015-08-02 20:13:28 +1000

    add cache resetable

---
+++ src/main/java/com/github/davidmoten/rx/operators/OnSubscribeCacheResetable.java
@@ -0,0 +1,32 @@
+package com.github.davidmoten.rx.operators;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Subscriber;
+
+public final class OnSubscribeCacheResetable<T> implements OnSubscribe<T> {
+
+    private final AtomicBoolean refresh = new AtomicBoolean(true);
+    private final Observable<T> source;
+    private volatile Observable<T> current;
+
+    public OnSubscribeCacheResetable(Observable<T> source) {
+        this.source = source;
+        this.current = source;
+    }
+
+    @Override
+    public void call(Subscriber<? super T> subscriber) {
+        if (refresh.compareAndSet(true, false)) {
+            current = source.cache();
+        }
+        current.unsafeSubscribe(subscriber);
+    }
+
+    public void reset() {
+        refresh.set(true);
+    }
+
+}
