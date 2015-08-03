09ed655d28231fd47748bf2e96adddad0f663744
Dave Moten <davidmoten@gmail.com>
2015-08-03 13:29:44 +1000

    fix whitespace

--- src/test/java/com/github/davidmoten/rx/operators/OnSubscribeCacheResetableTest.java
+++ src/test/java/com/github/davidmoten/rx/operators/OnSubscribeCacheResetableTest.java
@@ -19,17 +19,20 @@ public class OnSubscribeCacheResetableTest {
     public void test() {
         final AtomicInteger completedCount = new AtomicInteger();
         final AtomicInteger emissionCount = new AtomicInteger();
-        CachedObservable<Integer> cached = Obs.cache(Observable.just(1).doOnCompleted(new Action0() {
-            @Override
-            public void call() {
-                completedCount.incrementAndGet();
-            }}));
+        CachedObservable<Integer> cached = Obs.cache(Observable.just(1).doOnCompleted(
+                new Action0() {
+                    @Override
+                    public void call() {
+                        completedCount.incrementAndGet();
+                    }
+                }));
         Observable<Integer> o = cached.doOnNext(new Action1<Integer>() {
 
-                @Override
-                public void call(Integer n) {
-                        emissionCount.incrementAndGet();
-                }});
+            @Override
+            public void call(Integer n) {
+                emissionCount.incrementAndGet();
+            }
+        });
         o.subscribe();
         assertEquals(1, completedCount.get());
         assertEquals(1, emissionCount.get());
@@ -40,7 +43,6 @@ public class OnSubscribeCacheResetableTest {
         o.subscribe();
         assertEquals(2, completedCount.get());
         assertEquals(3, emissionCount.get());
-        
     }
-    
+
 }
