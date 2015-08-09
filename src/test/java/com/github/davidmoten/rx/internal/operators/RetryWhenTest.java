cd57abce7298b1e827491e4e2d6af693a10f00f4
Dave Moten <davidmoten@gmail.com>
2015-08-09 17:07:23 +1000

    add tests

--- src/test/java/com/github/davidmoten/rx/internal/operators/RetryWhenTest.java
+++ src/test/java/com/github/davidmoten/rx/internal/operators/RetryWhenTest.java
@@ -2,13 +2,14 @@ package com.github.davidmoten.rx.internal.operators;
 
 import static org.junit.Assert.assertEquals;
 
+import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Test;
 
 import com.github.davidmoten.rx.RetryWhen;
-import com.github.davidmoten.util.ErrorAndDuration;
+import com.github.davidmoten.rx.RetryWhen.ErrorAndDuration;
 
 import rx.Observable;
 import rx.functions.Action1;
@@ -52,14 +53,6 @@ public class RetryWhenTest {
         Exception ex = new IllegalArgumentException("boo");
         TestSubscriber<Integer> ts = TestSubscriber.create();
         TestScheduler scheduler = new TestScheduler();
-        Action1<ErrorAndDuration> log = new Action1<ErrorAndDuration>() {
-
-            @Override
-            public void call(ErrorAndDuration e) {
-                System.out.println("WARN: " + e.throwable().getMessage());
-                System.out.println("waiting for " + e.durationMs() + "ms");
-            }
-        };
         Observable.just(1, 2)
                 // force error after 3 emissions
                 .concatWith(Observable.<Integer> error(ex))
@@ -81,5 +74,71 @@ public class RetryWhenTest {
         ts.assertValues(1, 2, 1, 2, 1, 2);
         ts.assertError(ex);
     }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testRetryWhenSpecificExceptionFails() {
+        Exception ex = new IllegalArgumentException("boo");
+        TestSubscriber<Integer> ts = TestSubscriber.create();
+        TestScheduler scheduler = new TestScheduler();
+        Observable.just(1, 2)
+                // force error after 3 emissions
+                .concatWith(Observable.<Integer> error(ex))
+                // retry with backoff
+                .retryWhen(RetryWhen.maxRetries(2).action(log)
+                        .exponentialBackoff(1, TimeUnit.MINUTES).scheduler(scheduler)
+                        .failWhenInstanceOf(IllegalArgumentException.class).build())
+                // go
+                .subscribe(ts);
+        ts.assertValues(1, 2);
+        ts.assertError(ex);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testRetryWhenSpecificExceptionFailsBecauseIsNotInstanceOf() {
+        Exception ex = new IllegalArgumentException("boo");
+        TestSubscriber<Integer> ts = TestSubscriber.create();
+        TestScheduler scheduler = new TestScheduler();
+        Observable.just(1, 2)
+                // force error after 3 emissions
+                .concatWith(Observable.<Integer> error(ex))
+                // retry with backoff
+                .retryWhen(RetryWhen.maxRetries(2).action(log)
+                        .exponentialBackoff(1, TimeUnit.MINUTES).scheduler(scheduler)
+                        .retryWhenInstanceOf(SQLException.class).build())
+                // go
+                .subscribe(ts);
+        ts.assertValues(1, 2);
+        ts.assertError(ex);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testRetryWhenSpecificExceptionAllowed() {
+        Exception ex = new IllegalArgumentException("boo");
+        TestSubscriber<Integer> ts = TestSubscriber.create();
+        TestScheduler scheduler = new TestScheduler();
+        Observable.just(1, 2)
+                // force error after 3 emissions
+                .concatWith(Observable.<Integer> error(ex))
+                // retry with backoff
+                .retryWhen(RetryWhen.maxRetries(2).action(log)
+                        .exponentialBackoff(1, TimeUnit.MINUTES).scheduler(scheduler)
+                        .retryWhenInstanceOf(IllegalArgumentException.class).build())
+                // go
+                .subscribe(ts);
+        ts.assertValues(1, 2);
+        ts.assertNotCompleted();
+    }
+
+    private static final Action1<ErrorAndDuration> log = new Action1<ErrorAndDuration>() {
+
+        @Override
+        public void call(ErrorAndDuration e) {
+            System.out.println("WARN: " + e.throwable().getMessage());
+            System.out.println("waiting for " + e.durationMs() + "ms");
+        }
+    };
 
 }
