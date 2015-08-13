7656079c871243997e1f7402ba02578b40290de2
Dave Moten <davidmoten@gmail.com>
2015-08-13 21:24:47 +1000

    add Ob play

--- src/main/java/com/github/davidmoten/util/DelegatorGenerator.java
+++ src/main/java/com/github/davidmoten/util/DelegatorGenerator.java
@@ -3,6 +3,7 @@ package com.github.davidmoten.util;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +28,82 @@ public final class DelegatorGenerator {
 
                 System.out.format("%s %s %s %s(\n", mods, typeParams, g, method.getName());
             }
+        }
+    }
+
+    String sharedToGenericString(Method m, int modifierMask, boolean isDefault) {
+        try {
+            StringBuilder sb = new StringBuilder();
+
+            printModifiersIfNonzero(m, sb, modifierMask, isDefault);
+
+            TypeVariable<?>[] typeparms = m.getTypeParameters();
+            if (typeparms.length > 0) {
+                boolean first = true;
+                sb.append('<');
+                for (TypeVariable<?> typeparm : typeparms) {
+                    if (!first)
+                        sb.append(',');
+                    // Class objects can't occur here; no need to test
+                    // and call Class.getName().
+                    sb.append(typeparm.toString());
+                    first = false;
+                }
+                sb.append("> ");
+            }
+
+            specificToGenericStringHeader(m, sb);
+
+            sb.append('(');
+            Type[] params = m.getGenericParameterTypes();
+            for (int j = 0; j < params.length; j++) {
+                String param = params[j].getTypeName();
+                if (m.isVarArgs() && (j == params.length - 1)) // replace T[]
+                                                               // with
+                                                               // T...
+                    param = param.replaceFirst("\\[\\]$", "...");
+                sb.append(param);
+                if (j < (params.length - 1))
+                    sb.append(',');
+            }
+            sb.append(')');
+            Type[] exceptions = m.getGenericExceptionTypes();
+            if (exceptions.length > 0) {
+                sb.append(" throws ");
+                for (int k = 0; k < exceptions.length; k++) {
+                    sb.append((exceptions[k] instanceof Class) ? ((Class) exceptions[k]).getName()
+                            : exceptions[k].toString());
+                    if (k < (exceptions.length - 1))
+                        sb.append(',');
+                }
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            return "<" + e + ">";
+        }
+    }
+
+    void specificToGenericStringHeader(Method m, StringBuilder sb) {
+        Type genRetType = m.getGenericReturnType();
+        sb.append(genRetType.getTypeName()).append(' ');
+        sb.append(m.getDeclaringClass().getTypeName()).append('.');
+        sb.append(m.getName());
+    }
+
+    void printModifiersIfNonzero(Method m, StringBuilder sb, int mask, boolean isDefault) {
+        int mod = m.getModifiers() & mask;
+
+        if (mod != 0 && !isDefault) {
+            sb.append(Modifier.toString(mod)).append(' ');
+        } else {
+            int access_mod = mod & (Modifier.PUBLIC | Modifier.PROTECTED | Modifier.PRIVATE);
+            if (access_mod != 0)
+                sb.append(Modifier.toString(access_mod)).append(' ');
+            if (isDefault)
+                sb.append("default ");
+            mod = (mod & ~(Modifier.PUBLIC | Modifier.PROTECTED | Modifier.PRIVATE));
+            if (mod != 0)
+                sb.append(Modifier.toString(mod)).append(' ');
         }
     }
 
