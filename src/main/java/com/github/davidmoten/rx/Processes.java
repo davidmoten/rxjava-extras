02cd30a3fe01d2853c91e794d9e15b34db407bb8
Dave Moten <davidmoten@gmail.com>
2015-09-12 19:53:18 +1000

    rejig state machine so that completion emission is optional

---
+++ src/main/java/com/github/davidmoten/rx/Processes.java
@@ -0,0 +1,157 @@
+package com.github.davidmoten.rx;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.github.davidmoten.util.Optional;
+
+import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+public final class Processes {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        execute("ls").map(new Func1<byte[], String>() {
+
+            @Override
+            public String call(byte[] bytes) {
+                return new String(bytes);
+            }
+        });
+    }
+
+    public static Observable<byte[]> execute(String... command) {
+        return execute(
+                new Parameters(Arrays.asList(command), Optional.<Map<String, String>> absent(),
+                        true, new File("."), Optional.<Long> absent()));
+    }
+
+    public static Observable<byte[]> execute(final Parameters parameters) {
+        Func0<Process> resourceFactory = new Func0<Process>() {
+
+            @Override
+            public Process call() {
+                ProcessBuilder b = new ProcessBuilder(parameters.command());
+                if (parameters.env().isPresent()) {
+                    if (parameters.appendEnv())
+                        b.environment().clear();
+                    b.environment().putAll(parameters.env().get());
+                }
+                b.directory(parameters.directory());
+                b.redirectErrorStream(true);
+                try {
+                    return b.start();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        Func1<Process, Observable<byte[]>> factory = new Func1<Process, Observable<byte[]>>() {
+            @Override
+            public Observable<byte[]> call(final Process process) {
+                InputStream is = process.getInputStream();
+                Observable<byte[]> output;
+                if (is != null)
+                    output = Bytes.from(is);
+                else
+                    output = Observable.empty();
+                Observable<byte[]> completion = Observable.create(new OnSubscribe<byte[]>() {
+                    @Override
+                    public void call(Subscriber<? super byte[]> sub) {
+                        try {
+                            if (parameters.waitForMs().isPresent()) {
+                                boolean finished = process.waitFor(parameters.waitForMs().get(),
+                                        TimeUnit.MILLISECONDS);
+                                if (!finished) {
+                                    sub.onError(new TimeoutException("process timed out"));
+                                    return;
+                                }
+                            } else {
+                                int exitCode = process.waitFor();
+                                if (exitCode != 0)
+                                    sub.onError(new ProcessException(exitCode));
+                                return;
+                            }
+                            sub.onCompleted();
+                        } catch (InterruptedException e) {
+                            sub.onError(e);
+                        }
+                    }
+                }).subscribeOn(Schedulers.io());
+                return output.concatWith(completion);
+            }
+        };
+        Action1<? super Process> disposeAction = new Action1<Process>() {
+            @Override
+            public void call(Process process) {
+                if (process.isAlive())
+                    process.destroy();
+            }
+        };
+        return Observable.using(resourceFactory, factory, disposeAction);
+    }
+
+    public static class ProcessException extends RuntimeException {
+        private final int exitCode;
+
+        public ProcessException(int exitCode) {
+            super("process returned exitCode " + exitCode);
+            this.exitCode = exitCode;
+        }
+
+        public int exitCode() {
+            return exitCode;
+        }
+
+    }
+
+    public static final class Parameters {
+        private final List<String> command;
+        private final Optional<Map<String, String>> env;
+        private final boolean appendEnv;
+        private final File directory;
+        private final Optional<Long> waitForMs;
+
+        public Parameters(List<String> command, Optional<Map<String, String>> env,
+                boolean appendEnv, File directory, Optional<Long> waitForMs) {
+            this.command = command;
+            this.env = env;
+            this.appendEnv = appendEnv;
+            this.directory = directory;
+            this.waitForMs = waitForMs;
+        }
+
+        public Optional<Long> waitForMs() {
+            return waitForMs;
+        }
+
+        public File directory() {
+            return directory;
+        }
+
+        public List<String> command() {
+            return command;
+        }
+
+        public Optional<Map<String, String>> env() {
+            return env;
+        }
+
+        public boolean appendEnv() {
+            return appendEnv;
+        }
+
+    }
+}
---
+++ src/main/java/com/github/davidmoten/rx/internal/operators/TransformerDecode.java
@@ -0,0 +1,108 @@
+package com.github.davidmoten.rx.internal.operators;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
+
+import com.github.davidmoten.rx.Transformers;
+
+import rx.Observable.Transformer;
+import rx.Subscriber;
+import rx.functions.Func0;
+import rx.functions.Func2;
+import rx.functions.Func3;
+
+public class TransformerDecode {
+
+    public static Transformer<byte[], String> decode(final CharsetDecoder decoder) {
+        Func0<ByteBuffer> initialState = new Func0<ByteBuffer>() {
+
+            @Override
+            public ByteBuffer call() {
+                return null;
+            }
+        };
+        Func3<ByteBuffer, byte[], Subscriber<String>, ByteBuffer> transition = new Func3<ByteBuffer, byte[], Subscriber<String>, ByteBuffer>() {
+
+            @Override
+            public ByteBuffer call(ByteBuffer last, byte[] next, Subscriber<String> o) {
+                Result result = process(next, last, false, decoder, o);
+                return result.leftOver;
+            }
+        };
+        Func2<ByteBuffer, Subscriber<String>, Boolean> completion = new Func2<ByteBuffer, Subscriber<String>, Boolean>() {
+
+            @Override
+            public Boolean call(ByteBuffer last, Subscriber<String> subscriber) {
+                return process(null, last, true, decoder, subscriber).canEmitFurther;
+            }
+        };
+
+        return Transformers.stateMachine(initialState, transition, completion);
+    }
+
+    private static final class Result {
+        final ByteBuffer leftOver;
+        final boolean canEmitFurther;
+
+        Result(ByteBuffer leftOver, boolean canEmitFurther) {
+            this.leftOver = leftOver;
+            this.canEmitFurther = canEmitFurther;
+        }
+
+    }
+
+    public static Result process(byte[] next, ByteBuffer last, boolean endOfInput,
+            CharsetDecoder decoder, Subscriber<String> o) {
+        if (o.isUnsubscribed())
+            return new Result(null, false);
+
+        ByteBuffer bb;
+        if (last != null) {
+            if (next != null) {
+                // merge leftover in front of the next bytes
+                bb = ByteBuffer.allocate(last.remaining() + next.length);
+                bb.put(last);
+                bb.put(next);
+                bb.flip();
+            } else { // next == null
+                bb = last;
+            }
+        } else { // last == null
+            if (next != null) {
+                bb = ByteBuffer.wrap(next);
+            } else { // next == null
+                return new Result(null, true);
+            }
+        }
+
+        CharBuffer cb = CharBuffer.allocate((int) (bb.limit() * decoder.averageCharsPerByte()));
+        CoderResult cr = decoder.decode(bb, cb, endOfInput);
+        cb.flip();
+
+        if (cr.isError()) {
+            try {
+                cr.throwException();
+            } catch (CharacterCodingException e) {
+                o.onError(e);
+                return new Result(null, false);
+            }
+        }
+
+        ByteBuffer leftOver;
+        if (bb.remaining() > 0) {
+            leftOver = bb;
+        } else {
+            leftOver = null;
+        }
+
+        String string = cb.toString();
+        if (!string.isEmpty())
+            o.onNext(string);
+
+        return new Result(leftOver, true);
+    }
+
+}
