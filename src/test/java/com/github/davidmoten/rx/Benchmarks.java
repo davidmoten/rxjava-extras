8090ccfaa60d3c6045cf085f3818a6760f1ca7e9
Dave Moten <davidmoten@gmail.com>
2015-09-17 21:15:33 +1000

    fix benchmarks

--- src/test/java/com/github/davidmoten/rx/Benchmarks.java
+++ src/test/java/com/github/davidmoten/rx/Benchmarks.java
@@ -9,7 +9,7 @@ public class Benchmarks {
 
     private static final Observable<Integer> o1 = Observable.just(1);
     private static final Observable<Integer> o5 = Observable.just(1, 2, 3, 4, 5);
-    private static final Observable<Integer> o1000 = Observable.just(1);
+    private static final Observable<Integer> o1000 = Observable.range(1, 1000);
 
     @Benchmark
     public void concatOne() {
