8753b0437aa98d05130c6c8ea61997801858bbed
Dave Moten <davidmoten@gmail.com>
2015-11-12 13:36:34 +1100

    revert to 1.0.14

--- src/test/java/com/github/davidmoten/rx/MathsTest.java
+++ src/test/java/com/github/davidmoten/rx/MathsTest.java
@@ -14,7 +14,6 @@ import rx.functions.Func2;
 public class MathsTest {
 
     @Test
-    @org.junit.Ignore 
     public void testNewtonsSolver() {
         Func1<Double, Double> f = new Func1<Double, Double>() {
 
@@ -24,28 +23,27 @@ public class MathsTest {
             }
         };
         double x = Maths.solveWithNewtonsMethod(f, 1, 0.01).elementAt(100).toBlocking().single();
-        assertEquals(Math.pow(9,  0.33333333333333), x, 0.000001);
+        assertEquals(Math.pow(9, 0.33333333333333), x, 0.000001);
     }
-    
-    @Test(timeout=1000)
+
+    @Test(timeout = 1000)
     public void testScan() {
         Observable.range(1, Integer.MAX_VALUE)
-        //
-        .scan(1,
-                new Func2<Integer, Integer, Integer>() {
+                //
+                .scan(1, new Func2<Integer, Integer, Integer>() {
                     @Override
                     public Integer call(Integer x, Integer y) {
                         return x;
                     }
                 })
-        //
-        .elementAt(0).toBlocking().single();
+                //
+                .elementAt(0).toBlocking().single();
     }
-    
+
     @Test
     public void testPrimes() {
         List<Long> list = Maths.primes().take(10).toList().toBlocking().single();
-        assertEquals(Arrays.asList(1L,2L,3L,5L,7L,11L,13L,17L,19L,23L), list);
+        assertEquals(Arrays.asList(1L, 2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L), list);
     }
 
 }
