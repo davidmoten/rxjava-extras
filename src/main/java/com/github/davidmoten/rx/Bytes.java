7914135bedd8d4890c057430d4c27fcf88a2d8e7
Dave Moten <davidmoten@gmail.com>
2016-03-12 06:10:10 +1100

    add docs and simple test to Byts

--- src/main/java/com/github/davidmoten/rx/Bytes.java
+++ src/main/java/com/github/davidmoten/rx/Bytes.java
@@ -20,10 +20,31 @@ import rx.observables.SyncOnSubscribe;
 
 public final class Bytes {
 
+    private Bytes() {
+        // prevent instantiation
+    }
+
+    /**
+     * Returns an Observable stream of byte arrays from the given
+     * {@link InputStream} of {@code size} bytes. The final byte array may be less
+     * than {@code size} bytes.
+     * 
+     * @param is input stream of bytes
+     * @param size max emitted byte array size
+     * @return a stream of byte arrays
+     */
     public static Observable<byte[]> from(InputStream is, int size) {
         return Observable.create(new OnSubscribeInputStream(is, size));
     }
 
+    /**
+     * Returns an Observable stream of byte arrays from the given
+     * {@link InputStream} of {@code 8192} bytes. The final byte array may be less
+     * than {@code 8192} bytes.
+     * 
+     * @param is input stream of bytes
+     * @return a stream of byte arrays
+     */
     public static Observable<byte[]> from(InputStream is) {
         return from(is, 8192);
     }
@@ -64,7 +85,7 @@ public final class Bytes {
     }
 
     public static Observable<ZippedEntry> unzip(final ZipInputStream zis) {
-        return Observable.create(new SyncOnSubscribe<ZipInputStream,ZippedEntry>() {
+        return Observable.create(new SyncOnSubscribe<ZipInputStream, ZippedEntry>() {
 
             @Override
             protected ZipInputStream generateState() {
@@ -72,7 +93,8 @@ public final class Bytes {
             }
 
             @Override
-            protected ZipInputStream next(ZipInputStream zis, Observer<? super ZippedEntry> observer) {
+            protected ZipInputStream next(ZipInputStream zis,
+                    Observer<? super ZippedEntry> observer) {
                 try {
                     ZipEntry zipEntry = zis.getNextEntry();
                     if (zipEntry != null) {
