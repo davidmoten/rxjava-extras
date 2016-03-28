27d01ee1d2c687bd27598f65082ab349c40ceacb
Dave Moten <davidmoten@gmail.com>
2016-03-29 10:23:46 +1100

    buffer to file dev

---
+++ src/main/java/com/github/davidmoten/rx/internal/operators/OperatorBufferToFile.java
@@ -0,0 +1,165 @@
+package com.github.davidmoten.rx.internal.operators;
+
+import java.io.File;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.Serializer;
+
+import com.github.davidmoten.rx.subjects.PublishSubjectSingleSubscriber;
+
+import rx.Notification;
+import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Observable.Operator;
+import rx.Producer;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.internal.operators.BackpressureUtils;
+import rx.observers.Subscribers;
+
+public class OperatorBufferToFile<T> implements Operator<T, T> {
+
+    private static final String QUEUE_NAME = "q";
+
+    private final File file;
+    private final Serializer<Notification<T>> serializer;
+
+    public OperatorBufferToFile(File file, Serializer<Notification<T>> serializer) {
+        this.file = file;
+        this.serializer = serializer;
+    }
+
+    @Override
+    public Subscriber<? super T> call(Subscriber<? super T> child) {
+        final DB db = DBMaker.newFileDB(file).make();
+        final BlockingQueue<Notification<T>> queue = getQueue(db, serializer);
+        final AtomicReference<QueueProducer<T>> queueProducer = new AtomicReference<QueueProducer<T>>();
+        Subscriber<T> sub = new Subscriber<T>() {
+
+            // requests Long.MAX_VALUE by default
+
+            @Override
+            public void onCompleted() {
+                queue.offer(Notification.<T> createOnCompleted());
+                messageArrived();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                queue.offer(Notification.<T> createOnError(e));
+                messageArrived();
+            }
+
+            @Override
+            public void onNext(T t) {
+                queue.offer(Notification.createOnNext(t));
+                messageArrived();
+            }
+
+            private void messageArrived() {
+                queueProducer.get().drain();
+            }
+
+        };
+
+        Observable<T> source = Observable.create(new OnSubscribe<T>() {
+            @Override
+            public void call(Subscriber<? super T> child) {
+                QueueProducer<T> qp = new QueueProducer<T>(queue, child);
+                queueProducer.set(qp);
+                child.setProducer(qp);
+            }
+        });
+
+        Subscriber<T> wrappedChild = Subscribers.wrap(child);
+        child.add(sub);
+        child.add(new Subscription() {
+
+            @Override
+            public void unsubscribe() {
+                db.close();
+            }
+
+            @Override
+            public boolean isUnsubscribed() {
+                return db.isClosed();
+            }
+        });
+        source.subscribe(wrappedChild);
+        return sub;
+    }
+
+    private static class QueueProducer<T> extends AtomicLong implements Producer {
+
+        private static final long serialVersionUID = 2521533710633950102L;
+        private final BlockingQueue<Notification<T>> queue;
+        private final AtomicBoolean wip = new AtomicBoolean(false);
+        private final Subscriber<? super T> child;
+
+        QueueProducer(BlockingQueue<Notification<T>> queue, Subscriber<? super T> child) {
+            super();
+            this.queue = queue;
+            this.child = child;
+        }
+
+        @Override
+        public void request(long n) {
+            if (n > 0) {
+                BackpressureUtils.getAndAddRequest(this, n);
+                drain();
+            }
+        }
+
+        public void drain() {
+            // only one thread at a time
+            if (wip.compareAndSet(false, true)) {
+                try {
+                    long r = get();
+                    while (true) {
+                        while (r > 0) {
+                            if (child.isUnsubscribed()) {
+                                return;
+                            } else {
+                                Notification<T> notification = queue.poll();
+                                if (notification == null) {
+                                    // queue is empty
+                                    return;
+                                } else {
+                                    //there was a notification on the queue
+                                    notification.accept(child);
+                                    r--;
+                                }
+                            }
+                        }
+                        r = addAndGet(-r);
+                        if (r == 0L) {
+                            // we're done emitting the number requested so
+                            // return
+                            return;
+                        }
+                    }
+                } finally {
+                    wip.set(false);
+                }
+            }
+        }
+
+    }
+
+    private static <T> BlockingQueue<Notification<T>> getQueue(DB db,
+            Serializer<Notification<T>> serializer) {
+        synchronized (db) {
+            if (db.getCatalog().containsKey(QUEUE_NAME)) {
+                return db.getQueue(QUEUE_NAME);
+            } else {
+                return db.createQueue(QUEUE_NAME, serializer, false);
+            }
+        }
+    }
+
+}
--- src/main/java/com/github/davidmoten/rx/internal/operators/TransformerOnBackpressureBufferToFile.java
+++ src/main/java/com/github/davidmoten/rx/internal/operators/TransformerOnBackpressureBufferToFile.java
@@ -15,7 +15,6 @@ import rx.functions.Func1;
 
 public final class TransformerOnBackpressureBufferToFile<T> implements Transformer<T, T> {
 
-    private static final String QUEUE_NAME = "q";
     private final File file;
     private final Serializer<T> serializer;
 
@@ -26,38 +25,7 @@ public final class TransformerOnBackpressureBufferToFile<T> implements Transform
 
     @Override
     public Observable<T> call(Observable<T> o) {
-        return Observable.defer(new Func0<Observable<T>>() {
-
-            @Override
-            public Observable<T> call() {
-                Func0<DB> resourceFactory = new Func0<DB>() {
-                    @Override
-                    public DB call() {
-                        return DBMaker.newFileDB(file).make();
-                    }
-                };
-                Func1<DB, Observable<T>> observableFactory = new Func1<DB, Observable<T>>() {
-                    @Override
-                    public Observable<T> call(DB db) {
-                        final BlockingQueue<T> queue;
-                        if (db.getCatalog().containsKey(QUEUE_NAME)) {
-                            queue = db.getQueue(QUEUE_NAME);
-                        } else {
-                            queue = db.createQueue(QUEUE_NAME, serializer, false);
-                        }
-                        return Observable.create(new OnSubscribeFromQueue<T>(queue));
-                    }
-                };
-                Action1<DB> disposeAction = new Action1<DB>() {
-                    @Override
-                    public void call(DB db) {
-                        db.close();
-                    }
-                };
-                return Observable.using(resourceFactory, observableFactory, disposeAction, true)
-                        .repeat();
-            }
-        });
+        throw new RuntimeException("not implemented yet");
     }
 
     public static void main(String[] args) {
