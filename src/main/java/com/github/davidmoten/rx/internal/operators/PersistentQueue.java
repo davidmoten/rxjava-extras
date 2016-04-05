04417ce32e2ec206e3f20f84f151689e2d99c7b6
Dave Moten <davidmoten@gmail.com>
2016-04-05 21:12:34 +1000

    persistentqueue dev

--- src/main/java/com/github/davidmoten/rx/internal/operators/PersistentQueue.java
+++ src/main/java/com/github/davidmoten/rx/internal/operators/PersistentQueue.java
@@ -20,236 +20,251 @@ import com.github.davidmoten.util.Preconditions;
 
 class PersistentQueue<T> implements CloseableQueue<T> {
 
-    int readBufferPosition = 0;
-    volatile int readPosition = 0;
-    final byte[] readBuffer;
-    int readBufferLength = 0;
-    final byte[] writeBuffer;
-    final RandomAccessFile f;
-    final DataSerializer<T> serializer;
-    final DataOutput output;
-    final DataInput input;
-    final File file;
-    final AtomicLong size;
-    volatile int writePosition;
-    volatile int writeBufferPosition;
+	private static final boolean debug = false;
 
-    public PersistentQueue(int bufferSizeBytes, File file, DataSerializer<T> serializer) {
-        Preconditions.checkArgument(bufferSizeBytes > 0,
-                "bufferSizeBytes must be greater than zero");
-        Preconditions.checkNotNull(file);
-        Preconditions.checkArgument(!file.exists(), "file exists already");
-        Preconditions.checkNotNull(serializer);
-        this.readBuffer = new byte[bufferSizeBytes];
-        this.writeBuffer = new byte[bufferSizeBytes];
-        try {
-            file.getParentFile().mkdirs();
-            file.createNewFile();
-            this.file = file;
-            this.f = new RandomAccessFile(file, "rw");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        this.serializer = serializer;
-        this.output = new DataOutputStream(new QueueWriter(this));
-        this.input = new DataInputStream(new QueueReader(this));
-        this.size = new AtomicLong(0);
-    }
+	int readBufferPosition = 0;
+	volatile int readPosition = 0;
+	final byte[] readBuffer;
+	int readBufferLength = 0;
+	final byte[] writeBuffer;
+	final RandomAccessFile f;
+	final DataSerializer<T> serializer;
+	final DataOutput output;
+	final DataInput input;
+	final File file;
+	final AtomicLong size;
+	volatile int writePosition;
+	volatile int writeBufferPosition;
+	private final Object fileLock = new Object();
+	private final Object writePositionLock = new Object();
 
-    private static class QueueWriter extends OutputStream {
+	public PersistentQueue(int bufferSizeBytes, File file, DataSerializer<T> serializer) {
+		Preconditions.checkArgument(bufferSizeBytes > 0, "bufferSizeBytes must be greater than zero");
+		Preconditions.checkNotNull(file);
+		Preconditions.checkArgument(!file.exists(), "file exists already");
+		Preconditions.checkNotNull(serializer);
+		this.readBuffer = new byte[bufferSizeBytes];
+		this.writeBuffer = new byte[bufferSizeBytes];
+		try {
+			file.getParentFile().mkdirs();
+			file.createNewFile();
+			this.file = file;
+			this.f = new RandomAccessFile(file, "rw");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		this.serializer = serializer;
+		this.output = new DataOutputStream(new QueueWriter(this));
+		this.input = new DataInputStream(new QueueReader(this));
+		this.size = new AtomicLong(0);
+	}
 
-        private final PersistentQueue<?> q;
+	static void log(String string) {
+		if (debug)
+			System.out.println(string);
+	}
 
-        QueueWriter(PersistentQueue<?> queue) {
-            this.q = queue;
-        }
+	private static class QueueWriter extends OutputStream {
 
-        @Override
-        public void write(int b) throws IOException {
-            if (q.writeBufferPosition < q.writeBuffer.length) {
-                System.out.println("writeBuffer[" + q.writeBufferPosition + "]=" + b);
-                q.writeBuffer[q.writeBufferPosition] = (byte) b;
-                q.writeBufferPosition++;
-            } else {
-                synchronized (this) {
-                    q.f.seek(q.writePosition);
-                    q.f.write(q.writeBuffer);
-                    System.out.println("wrote buffer " + Arrays.toString(q.writeBuffer));
-                }
-                q.writeBuffer[0] = (byte) b;
-                q.writeBufferPosition = 1;
-                q.writePosition += q.writeBuffer.length;
-            }
-        }
+		private final PersistentQueue<?> q;
 
-    }
+		QueueWriter(PersistentQueue<?> queue) {
+			this.q = queue;
+		}
 
-    private static class QueueReader extends InputStream {
+		@Override
+		public void write(int b) throws IOException {
+			if (q.writeBufferPosition < q.writeBuffer.length) {
+				log("writeBuffer[" + q.writeBufferPosition + "]=" + b);
+				q.writeBuffer[q.writeBufferPosition] = (byte) b;
+				q.writeBufferPosition++;
+			} else {
+				synchronized (q.fileLock) {
+					q.f.seek(q.writePosition);
+					q.f.write(q.writeBuffer);
+					log("wrote buffer " + Arrays.toString(q.writeBuffer));
+				}
 
-        private final PersistentQueue<?> q;
+				q.writeBuffer[0] = (byte) b;
+				q.writeBufferPosition = 1;
+				q.writePosition += q.writeBuffer.length;
+			}
+		}
 
-        QueueReader(PersistentQueue<?> queue) {
-            this.q = queue;
-        }
+	}
 
-        @Override
-        public int read() throws IOException {
-            if (q.size.get() == 0) {
-                throw new EOFException();
-            } else {
-                if (q.readBufferPosition < q.readBufferLength) {
-                    int b = q.readBuffer[q.readBufferPosition];
-                    System.out
-                            .println("returned from readBuffer[" + q.readBufferPosition + "]=" + b);
-                    q.readBufferPosition++;
-                    return b;
-                } else {
-                    while (true) {
-                        int wp = q.writePosition;
-                        int wbp = q.writeBufferPosition;
-                        int over = wp - q.readPosition;
-                        if (over > 0) {
-                            synchronized (this) {
-                                q.f.seek(q.readPosition);
-                                q.readBufferLength = Math.min(q.readBuffer.length, over);
-                                q.f.read(q.readBuffer, 0, q.readBufferLength);
-                                System.out.println("read buffer " + Arrays.toString(q.readBuffer));
-                            }
-                            q.readPosition += q.readBufferLength;
-                            q.readBufferPosition = 1;
-                            System.out.println("returned from readBuffer[0]=" + q.readBuffer[0]);
-                            return q.readBuffer[0];
-                        } else {
-                            int b = q.writeBuffer[-over];
-                            if (wp == q.writePosition && wbp == q.writeBufferPosition) {
-                                q.readPosition++;
-                                System.out
-                                        .println("returned from writeBuffer[" + (-over) + "]=" + b);
-                                return b;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+	private static class QueueReader extends InputStream {
 
-    }
+		private final PersistentQueue<?> q;
 
-    @Override
-    public void close() {
-        try {
-            f.getChannel().close();
-            if (!file.delete()) {
-                throw new RuntimeException("could not delete file " + file);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+		QueueReader(PersistentQueue<?> queue) {
+			this.q = queue;
+		}
 
-    @Override
-    public boolean offer(T t) {
-        try {
-            serializer.serialize(output, t);
-            size.incrementAndGet();
-            return true;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+		@Override
+		public int read() throws IOException {
+			if (q.size.get() == 0) {
+				throw new EOFException();
+			} else {
+				if (q.readBufferPosition < q.readBufferLength) {
+					byte b = q.readBuffer[q.readBufferPosition];
+					log("returned from readBuffer[" + q.readBufferPosition + "]=" + b);
+					q.readBufferPosition++;
+					return toUnsignedInteger(b);
+				} else {
+					int wp;
+					int wbp;
+					synchronized (q.writePositionLock) {
+						wp = q.writePosition;
+						wbp = q.writeBufferPosition;
+					}
+					int over = wp - q.readPosition;
+					if (over > 0) {
+						synchronized (q.fileLock) {
+							q.f.seek(q.readPosition);
+							q.readBufferLength = Math.min(q.readBuffer.length, over);
+							q.f.read(q.readBuffer, 0, q.readBufferLength);
+						}
+						log("read buffer " + Arrays.toString(q.readBuffer));
+						q.readPosition += q.readBufferLength;
+						q.readBufferPosition = 1;
+						log("returned from readBuffer[0]=" + q.readBuffer[0]);
+						return toUnsignedInteger(q.readBuffer[0]);
+					} else {
+						int index = -over;
+						if (index >= q.writeBuffer.length) {
+							throw new EOFException();
+						} else {
+							int b = toUnsignedInteger(q.writeBuffer[-over]);
+							q.readPosition++;
+							log("returned from writeBuffer[" + index + "]=" + b);
+							return b;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	private static int toUnsignedInteger(byte b) {
+		return (int)b & 0x000000FF;
+	}
 
-    @Override
-    public T poll() {
-        try {
-            T t = serializer.deserialize(input, -1);
-            size.decrementAndGet();
-            return t;
-        } catch (EOFException e) {
-            return null;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@Override
+	public void close() {
+		try {
+			f.getChannel().close();
+			if (!file.delete()) {
+				throw new RuntimeException("could not delete file " + file);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @Override
-    public boolean isEmpty() {
-        return size.get() == 0;
-    }
+	@Override
+	public boolean offer(T t) {
+		try {
+			serializer.serialize(output, t);
+			size.incrementAndGet();
+			return true;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @Override
-    public T element() {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public T poll() {
+		try {
+			T t = serializer.deserialize(input, -1);
+			size.decrementAndGet();
+			return t;
+		} catch (EOFException e) {
+			return null;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @Override
-    public T peek() {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean isEmpty() {
+		return size.get() == 0;
+	}
 
-    @Override
-    public int size() {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public T element() {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean add(T e) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public T peek() {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public T remove() {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public int size() {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean contains(Object o) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean add(T e) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public Iterator<T> iterator() {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public T remove() {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public Object[] toArray() {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean contains(Object o) {
+		throw new UnsupportedOperationException();
+	}
 
-    @SuppressWarnings("hiding")
-    @Override
-    public <T> T[] toArray(T[] a) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public Iterator<T> iterator() {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public Object[] toArray() {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
+	@SuppressWarnings("hiding")
+	@Override
+	public <T> T[] toArray(T[] a) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean remove(Object o) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean addAll(Collection<? extends T> c) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
 
 }
--- src/test/java/com/github/davidmoten/rx/internal/operators/PersistentQueueTest.java
+++ src/test/java/com/github/davidmoten/rx/internal/operators/PersistentQueueTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import java.io.File;
+import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
 
@@ -11,43 +12,79 @@ import com.github.davidmoten.rx.buffertofile.DataSerializers;
 
 public class PersistentQueueTest {
 
-    @Test
-    public void test() {
-        PersistentQueue<Integer> q = createQueue();
-        q.offer(1);
-        assertEquals(1, (int) q.poll());
-        assertNull(q.poll());
-    }
-    
-    @Test
-    public void test2() {
-        PersistentQueue<Integer> q = createQueue();
-        q.offer(1);
-        q.offer(2);
-        assertEquals(1, (int) q.poll());
-        assertEquals(2, (int) q.poll());
-        assertNull(q.poll());
-    }
-    
-    @Test
-    public void test3() {
-        PersistentQueue<Integer> q = createQueue();
-        assertNull(q.poll());
-        q.offer(1);
-        q.offer(2);
-        assertEquals(1, (int) q.poll());
-        q.offer(3);
-        assertEquals(2, (int) q.poll());
-        assertEquals(3, (int) q.poll());
-        assertNull(q.poll());
-    }
+	@Test
+	public void test() {
+		PersistentQueue<Integer> q = createQueue();
+		q.offer(1);
+		assertEquals(1, (int) q.poll());
+		assertNull(q.poll());
+	}
 
-    private static PersistentQueue<Integer> createQueue() {
-        File file = new File("target/pq");
-        file.delete();
-        PersistentQueue<Integer> q = new PersistentQueue<Integer>(5, file,
-                DataSerializers.integer());
-        return q;
-    }
+	@Test
+	public void test2() {
+		PersistentQueue<Integer> q = createQueue();
+		q.offer(1);
+		q.offer(2);
+		assertEquals(1, (int) q.poll());
+		assertEquals(2, (int) q.poll());
+		assertNull(q.poll());
+	}
+
+	@Test
+	public void test3() {
+		PersistentQueue<Integer> q = createQueue();
+		assertNull(q.poll());
+		q.offer(1);
+		q.offer(2);
+		assertEquals(1, (int) q.poll());
+		q.offer(3);
+		assertEquals(2, (int) q.poll());
+		assertEquals(3, (int) q.poll());
+		assertNull(q.poll());
+	}
+
+	@Test
+	public void testConcurrent() throws InterruptedException, ExecutionException {
+		File file = new File("target/pq2");
+		file.delete();
+		final PersistentQueue<Integer> queue = new PersistentQueue<Integer>(10, file, DataSerializers.integer());
+		final int max = 10000000;
+		long t = System.currentTimeMillis();
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 1; i <= max; i++) {
+					queue.offer(i);
+				}
+			}
+		});
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				int i = 1;
+				while (i <= max) {
+					Integer t = queue.poll();
+					if (t != null) {
+						assertEquals(i, (int) t);
+						i++;
+					}
+				}
+			}
+		});
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println(Math.round(max * 1000.0 / (System.currentTimeMillis() - t)) + " per second");
+	}
+
+	private static PersistentQueue<Integer> createQueue() {
+		File file = new File("target/pq");
+		file.delete();
+		PersistentQueue<Integer> q = new PersistentQueue<Integer>(5, file, DataSerializers.integer());
+		return q;
+	}
 
 }
