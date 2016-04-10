c78aea92d48750a83f5b313f33f409f84cd1cec8
Dave Moten <davidmoten@gmail.com>
2016-04-10 12:14:42 +1000

    add test-long.sh script

---
+++ test-long.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo this takes about 12 minutes and tests bufferOnFile more intensively than
+echo a standard build.
+mvn clean install -Dmax.small=100000000 -Dmax.medium=300000 -Dmax.seconds=600
+
