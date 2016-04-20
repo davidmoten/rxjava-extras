e492cc5111e2a98222387195e95c9f6a6031b36a
Dave Moten <davidmoten@gmail.com>
2016-04-21 06:35:25 +1000

    clean up test-long.sh

--- test-long.sh
+++ test-long.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo this takes about 20 minutes and tests bufferOnFile more intensively than
-echo a standard build.
+echo ---------------------------------------------
+echo this takes about 20 minutes and tests 
+echo Transformer.onBackpressureBufferToFile more 
+echo intensively than a standard build.
+echo ---------------------------------------------
 mvn clean install -Dmax.small=100000000 -Dmax.medium=300000 -Dmax.seconds=600 -Dloops=10000
 
 
