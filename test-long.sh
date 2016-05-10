975362dbe0935d0ac0fafe65e5011caa08603248
Dave Moten <davidmoten@gmail.com>
2016-05-10 19:40:27 +1000

    add logging

--- test-long.sh
+++ test-long.sh
@@ -4,6 +4,6 @@ echo this takes about 35 minutes and tests
 echo Transformer.onBackpressureBufferToFile more 
 echo intensively than a standard build.
 echo ---------------------------------------------
-mvn clean install -Dmax.small=100000000 -Dmax.medium=300000 -Dmax.seconds=600 -Dloops=10000
+mvn clean install -Dmax.small=100000000 -Dmax.medium=3000000 -Dmax.seconds=600 -Dloops=10000
 
 
