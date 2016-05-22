92cdf5c18df2b13c6687bdfecdd5b34f3827224b
Dave Moten <davidmoten@gmail.com>
2016-05-22 10:55:23 +1000

    add class

---
+++ src/main/java/com/github/davidmoten/rx/internal/operators/TransformerRefCountDelayed.java
@@ -0,0 +1,14 @@
+package com.github.davidmoten.rx.internal.operators;
+
+import rx.Observable;
+import rx.Observable.Transformer;
+
+public class TransformerRefCountDelayed<T> implements Transformer<T,T>{
+
+	@Override
+	public Observable<T> call(Observable<T> t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
