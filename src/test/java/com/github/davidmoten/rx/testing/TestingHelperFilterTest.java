c35c6c3b4e9bef9dc558d21d8db99d5881189110
Dave Moten <davidmoten@gmail.com>
2016-06-09 12:32:03 +1000

    add Filter test

--- src/test/java/com/github/davidmoten/rx/testing/TestingHelperFilterTest.java
+++ src/test/java/com/github/davidmoten/rx/testing/TestingHelperFilterTest.java
@@ -12,26 +12,25 @@ public class TestingHelperFilterTest extends TestCase {
             return o.filter(new Func1<Integer, Boolean>() {
                 @Override
                 public Boolean call(Integer x) {
-                    return x % 2 ==0;
+                    return x % 2 == 0;
                 }
             });
         }
     };
 
     public static TestSuite suite() {
-        return TestingHelper.function(FILTER)
-                .name("testFilterOfEmptyReturnsEmpty").fromEmpty().expectEmpty() //
+        return TestingHelper.function(FILTER).name("testFilterOfEmptyReturnsEmpty").fromEmpty()
+                .expectEmpty() //
                 .name("testFilterOfOddReturnsEmpty").from(1).expectEmpty() //
                 .name("testFilterOfEventReturnsEven").from(2).expect(2) //
-                .name("testListReturnsEvens").from(1,2,3,4,5,6,7).expect(2,4,6)
+                .name("testListReturnsEvens").from(1, 2, 3, 4, 5, 6, 7).expect(2, 4, 6)
                 .name("testErrorReturnsError").fromError().expectError() //
                 // get test suites
                 .testSuite(TestingHelperFilterTest.class);
     }
-    
 
     public void testDummy() {
         // just here to fool eclipse
     }
-    
+
 }
