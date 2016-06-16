51767950c7a65ea04f857de833d7ae0872ff01e4
Dave Moten <davidmoten@gmail.com>
2016-06-16 17:44:36 +1000

    reduce allocations for Transformations.split by not wrapping left over with a dedicated class

--- src/main/java/com/github/davidmoten/rx/internal/operators/TransformerStringSplit.java
+++ src/main/java/com/github/davidmoten/rx/internal/operators/TransformerStringSplit.java
@@ -13,11 +13,11 @@ import rx.functions.Func3;
 public final class TransformerStringSplit {
 
 	public static <T> Transformer<String, String> split(final String pattern, final Pattern compiledPattern) {
-		Func0<LeftOver> initialState = Functions.constant0(new LeftOver(null));
-		Func3<LeftOver, String, Subscriber<String>, LeftOver> transition = new Func3<LeftOver, String, Subscriber<String>, LeftOver>() {
+		Func0<String> initialState = Functions.constant0(null);
+		Func3<String, String, Subscriber<String>, String> transition = new Func3<String, String, Subscriber<String>, String>() {
 
 			@Override
-			public LeftOver call(LeftOver leftOver, String s, Subscriber<String> observer) {
+			public String call(String leftOver, String s, Subscriber<String> observer) {
 				String[] parts;
 				if (compiledPattern != null) {
 					parts = compiledPattern.split(s, -1);
@@ -25,8 +25,8 @@ public final class TransformerStringSplit {
 					parts = s.split(pattern, -1);
 				}
 				// prepend leftover to the first part
-				if (leftOver.value != null)
-					parts[0] = leftOver.value + parts[0];
+				if (leftOver != null)
+					parts[0] = leftOver + parts[0];
 
 				// can emit all parts except the last part because it hasn't
 				// been terminated by the pattern/end-of-stream yet
@@ -40,16 +40,16 @@ public final class TransformerStringSplit {
 
 				// we have to assign the last part as leftOver because we
 				// don't know if it has been terminated yet
-				return new LeftOver(parts[parts.length - 1]);
+				return parts[parts.length - 1];
 			}
 		};
 
-		Func2<LeftOver, Subscriber<String>, Boolean> completion = new Func2<LeftOver, Subscriber<String>, Boolean>() {
+		Func2<String, Subscriber<String>, Boolean> completion = new Func2<String, Subscriber<String>, Boolean>() {
 
 			@Override
-			public Boolean call(LeftOver leftOver, Subscriber<String> observer) {
-				if (leftOver.value != null && !observer.isUnsubscribed())
-					observer.onNext(leftOver.value);
+			public Boolean call(String leftOver, Subscriber<String> observer) {
+				if (leftOver != null && !observer.isUnsubscribed())
+					observer.onNext(leftOver);
 				// TODO is this check needed?
 				if (!observer.isUnsubscribed())
 					observer.onCompleted();
@@ -57,14 +57,6 @@ public final class TransformerStringSplit {
 			}
 		};
 		return com.github.davidmoten.rx.Transformers.stateMachine(initialState, transition, completion);
-	}
-
-	private static class LeftOver {
-		final String value;
-
-		private LeftOver(String value) {
-			this.value = value;
-		}
 	}
 
 }
