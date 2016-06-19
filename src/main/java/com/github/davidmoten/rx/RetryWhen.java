52e7bfa9ef4c3d884580d028b8e62781628bec09
Dave Moten <davidmoten@gmail.com>
2016-06-20 08:14:56 +1000

    add RetryWhen.delaysInt

--- src/main/java/com/github/davidmoten/rx/RetryWhen.java
+++ src/main/java/com/github/davidmoten/rx/RetryWhen.java
@@ -37,258 +37,273 @@ import rx.schedulers.Schedulers;
  */
 public final class RetryWhen {
 
-    private static final long NO_MORE_DELAYS = -1;
+	private static final long NO_MORE_DELAYS = -1;
 
-    private static Func1<Observable<? extends Throwable>, Observable<?>> notificationHandler(
-            final Observable<Long> delays, final Scheduler scheduler,
-            final Action1<? super ErrorAndDuration> action,
-            final List<Class<? extends Throwable>> retryExceptions,
-            final List<Class<? extends Throwable>> failExceptions,
-            final Func1<? super Throwable, Boolean> exceptionPredicate) {
+	private static Func1<Observable<? extends Throwable>, Observable<?>> notificationHandler(
+			final Observable<Long> delays, final Scheduler scheduler, final Action1<? super ErrorAndDuration> action,
+			final List<Class<? extends Throwable>> retryExceptions,
+			final List<Class<? extends Throwable>> failExceptions,
+			final Func1<? super Throwable, Boolean> exceptionPredicate) {
 
-        final Func1<ErrorAndDuration, Observable<ErrorAndDuration>> checkExceptions = createExceptionChecker(
-                retryExceptions, failExceptions, exceptionPredicate);
+		final Func1<ErrorAndDuration, Observable<ErrorAndDuration>> checkExceptions = createExceptionChecker(
+				retryExceptions, failExceptions, exceptionPredicate);
 
-        return createNotificationHandler(delays, scheduler, action, checkExceptions);
-    }
+		return createNotificationHandler(delays, scheduler, action, checkExceptions);
+	}
 
-    private static Func1<Observable<? extends Throwable>, Observable<?>> createNotificationHandler(
-            final Observable<Long> delays, final Scheduler scheduler,
-            final Action1<? super ErrorAndDuration> action,
-            final Func1<ErrorAndDuration, Observable<ErrorAndDuration>> checkExceptions) {
-        return new Func1<Observable<? extends Throwable>, Observable<?>>() {
+	private static Func1<Observable<? extends Throwable>, Observable<?>> createNotificationHandler(
+			final Observable<Long> delays, final Scheduler scheduler, final Action1<? super ErrorAndDuration> action,
+			final Func1<ErrorAndDuration, Observable<ErrorAndDuration>> checkExceptions) {
+		return new Func1<Observable<? extends Throwable>, Observable<?>>() {
 
-            @Override
-            public Observable<ErrorAndDuration> call(Observable<? extends Throwable> errors) {
-                return errors
-                        // zip with delays, use -1 to signal completion
-                        .zipWith(delays.concatWith(just(NO_MORE_DELAYS)), TO_ERROR_AND_DURATION)
-                        // check retry and non-retry exceptions
-                        .flatMap(checkExceptions)
-                        // perform user action (for example log that a
-                        // delay is happening)
-                        .doOnNext(callActionExceptForLast(action))
-                        // delay the time in ErrorAndDuration
-                        .flatMap(delay(scheduler));
-            }
-        };
-    }
+			@Override
+			public Observable<ErrorAndDuration> call(Observable<? extends Throwable> errors) {
+				return errors
+						// zip with delays, use -1 to signal completion
+						.zipWith(delays.concatWith(just(NO_MORE_DELAYS)), TO_ERROR_AND_DURATION)
+						// check retry and non-retry exceptions
+						.flatMap(checkExceptions)
+						// perform user action (for example log that a
+						// delay is happening)
+						.doOnNext(callActionExceptForLast(action))
+						// delay the time in ErrorAndDuration
+						.flatMap(delay(scheduler));
+			}
+		};
+	}
 
-    private static Action1<ErrorAndDuration> callActionExceptForLast(
-            final Action1<? super ErrorAndDuration> action) {
-        return new Action1<ErrorAndDuration>() {
+	private static Action1<ErrorAndDuration> callActionExceptForLast(final Action1<? super ErrorAndDuration> action) {
+		return new Action1<ErrorAndDuration>() {
 
-            @Override
-            public void call(ErrorAndDuration e) {
-                if (e.durationMs() != NO_MORE_DELAYS)
-                    action.call(e);
-            }
+			@Override
+			public void call(ErrorAndDuration e) {
+				if (e.durationMs() != NO_MORE_DELAYS)
+					action.call(e);
+			}
 
-        };
-    }
+		};
+	}
 
-    // TODO unit test
-    private static Func1<ErrorAndDuration, Observable<ErrorAndDuration>> createExceptionChecker(
-            final List<Class<? extends Throwable>> retryExceptions,
-            final List<Class<? extends Throwable>> failExceptions,
-            final Func1<? super Throwable, Boolean> exceptionPredicate) {
-        return new Func1<ErrorAndDuration, Observable<ErrorAndDuration>>() {
+	// TODO unit test
+	private static Func1<ErrorAndDuration, Observable<ErrorAndDuration>> createExceptionChecker(
+			final List<Class<? extends Throwable>> retryExceptions,
+			final List<Class<? extends Throwable>> failExceptions,
+			final Func1<? super Throwable, Boolean> exceptionPredicate) {
+		return new Func1<ErrorAndDuration, Observable<ErrorAndDuration>>() {
 
-            @Override
-            public Observable<ErrorAndDuration> call(ErrorAndDuration e) {
-                if (!exceptionPredicate.call(e.throwable()))
-                    return Observable.error(e.throwable());
-                for (Class<? extends Throwable> cls : failExceptions) {
-                    if (e.throwable().getClass().isAssignableFrom(cls))
-                        return Observable.error(e.throwable());
-                }
-                if (retryExceptions.size() > 0) {
-                    for (Class<? extends Throwable> cls : retryExceptions) {
-                        if (e.throwable().getClass().isAssignableFrom(cls))
-                            return just(e);
-                    }
-                    return Observable.error(e.throwable());
-                } else {
-                    return just(e);
-                }
-            }
-        };
-    }
+			@Override
+			public Observable<ErrorAndDuration> call(ErrorAndDuration e) {
+				if (!exceptionPredicate.call(e.throwable()))
+					return Observable.error(e.throwable());
+				for (Class<? extends Throwable> cls : failExceptions) {
+					if (e.throwable().getClass().isAssignableFrom(cls))
+						return Observable.error(e.throwable());
+				}
+				if (retryExceptions.size() > 0) {
+					for (Class<? extends Throwable> cls : retryExceptions) {
+						if (e.throwable().getClass().isAssignableFrom(cls))
+							return just(e);
+					}
+					return Observable.error(e.throwable());
+				} else {
+					return just(e);
+				}
+			}
+		};
+	}
 
-    private static Func2<Throwable, Long, ErrorAndDuration> TO_ERROR_AND_DURATION = new Func2<Throwable, Long, ErrorAndDuration>() {
-        @Override
-        public ErrorAndDuration call(Throwable throwable, Long durationMs) {
-            return new ErrorAndDuration(throwable, durationMs);
-        }
-    };
+	private static Func2<Throwable, Long, ErrorAndDuration> TO_ERROR_AND_DURATION = new Func2<Throwable, Long, ErrorAndDuration>() {
+		@Override
+		public ErrorAndDuration call(Throwable throwable, Long durationMs) {
+			return new ErrorAndDuration(throwable, durationMs);
+		}
+	};
 
-    private static Func1<ErrorAndDuration, Observable<ErrorAndDuration>> delay(
-            final Scheduler scheduler) {
-        return new Func1<ErrorAndDuration, Observable<ErrorAndDuration>>() {
-            @Override
-            public Observable<ErrorAndDuration> call(ErrorAndDuration e) {
-                if (e.durationMs() == NO_MORE_DELAYS)
-                    return Observable.error(e.throwable());
-                else
-                    return Observable.timer(e.durationMs(), TimeUnit.MILLISECONDS, scheduler)
-                            .map(Functions.constant(e));
-            }
-        };
-    }
+	private static Func1<ErrorAndDuration, Observable<ErrorAndDuration>> delay(final Scheduler scheduler) {
+		return new Func1<ErrorAndDuration, Observable<ErrorAndDuration>>() {
+			@Override
+			public Observable<ErrorAndDuration> call(ErrorAndDuration e) {
+				if (e.durationMs() == NO_MORE_DELAYS)
+					return Observable.error(e.throwable());
+				else
+					return Observable.timer(e.durationMs(), TimeUnit.MILLISECONDS, scheduler)
+							.map(Functions.constant(e));
+			}
+		};
+	}
 
-    // Builder factory methods
+	// Builder factory methods
 
-    public static Builder retryWhenInstanceOf(Class<? extends Throwable>... classes) {
-        return new Builder().retryWhenInstanceOf(classes);
-    }
+	public static Builder retryWhenInstanceOf(Class<? extends Throwable>... classes) {
+		return new Builder().retryWhenInstanceOf(classes);
+	}
 
-    public static Builder failWhenInstanceOf(Class<? extends Throwable>... classes) {
-        return new Builder().failWhenInstanceOf(classes);
-    }
+	public static Builder failWhenInstanceOf(Class<? extends Throwable>... classes) {
+		return new Builder().failWhenInstanceOf(classes);
+	}
 
-    public static Builder retryIf(Func1<Throwable, Boolean> predicate) {
-        return new Builder().retryIf(predicate);
-    }
+	public static Builder retryIf(Func1<Throwable, Boolean> predicate) {
+		return new Builder().retryIf(predicate);
+	}
 
-    public static Builder delays(Observable<Long> delays, TimeUnit unit) {
-        return new Builder().delays(delays, unit);
-    }
+	public static Builder delays(Observable<Long> delays, TimeUnit unit) {
+		return new Builder().delays(delays, unit);
+	}
 
-    public static Builder delay(long delay, final TimeUnit unit) {
-        return new Builder().delay(delay, unit);
-    }
+	public static Builder delaysInt(Observable<Integer> delays, TimeUnit unit) {
+		return new Builder().delaysInt(delays, unit);
+	}
 
-    public static Builder maxRetries(int maxRetries) {
-        return new Builder().maxRetries(maxRetries);
-    }
+	public static Builder delay(long delay, final TimeUnit unit) {
+		return new Builder().delay(delay, unit);
+	}
 
-    public static Builder scheduler(Scheduler scheduler) {
-        return new Builder().scheduler(scheduler);
-    }
+	public static Builder maxRetries(int maxRetries) {
+		return new Builder().maxRetries(maxRetries);
+	}
 
-    public Builder action(Action1<? super ErrorAndDuration> action) {
-        return new Builder().action(action);
-    }
+	public static Builder scheduler(Scheduler scheduler) {
+		return new Builder().scheduler(scheduler);
+	}
 
-    public static Builder exponentialBackoff(final long firstDelay, final TimeUnit unit,
-            final double factor) {
-        return new Builder().exponentialBackoff(firstDelay, unit, factor);
-    }
+	public Builder action(Action1<? super ErrorAndDuration> action) {
+		return new Builder().action(action);
+	}
 
-    public static Builder exponentialBackoff(long firstDelay, TimeUnit unit) {
-        return new Builder().exponentialBackoff(firstDelay, unit);
-    }
+	public static Builder exponentialBackoff(final long firstDelay, final TimeUnit unit, final double factor) {
+		return new Builder().exponentialBackoff(firstDelay, unit, factor);
+	}
 
-    public static final class Builder {
+	public static Builder exponentialBackoff(long firstDelay, TimeUnit unit) {
+		return new Builder().exponentialBackoff(firstDelay, unit);
+	}
 
-        private final List<Class<? extends Throwable>> retryExceptions = new ArrayList<Class<? extends Throwable>>();
-        private final List<Class<? extends Throwable>> failExceptions = new ArrayList<Class<? extends Throwable>>();
-        private Func1<? super Throwable, Boolean> exceptionPredicate = Functions.alwaysTrue();
+	public static final class Builder {
 
-        private Observable<Long> delays = Observable.just(0L).repeat();
-        private Optional<Integer> maxRetries = absent();
-        private Optional<Scheduler> scheduler = of(Schedulers.computation());
-        private Action1<? super ErrorAndDuration> action = Actions.doNothing1();
+		private final List<Class<? extends Throwable>> retryExceptions = new ArrayList<Class<? extends Throwable>>();
+		private final List<Class<? extends Throwable>> failExceptions = new ArrayList<Class<? extends Throwable>>();
+		private Func1<? super Throwable, Boolean> exceptionPredicate = Functions.alwaysTrue();
 
-        private Builder() {
-            // must use static factory method to instantiate
-        }
+		private Observable<Long> delays = Observable.just(0L).repeat();
+		private Optional<Integer> maxRetries = absent();
+		private Optional<Scheduler> scheduler = of(Schedulers.computation());
+		private Action1<? super ErrorAndDuration> action = Actions.doNothing1();
 
-        public Builder retryWhenInstanceOf(Class<? extends Throwable>... classes) {
-            retryExceptions.addAll(Arrays.asList(classes));
-            return this;
-        }
+		private Builder() {
+			// must use static factory method to instantiate
+		}
 
-        public Builder failWhenInstanceOf(Class<? extends Throwable>... classes) {
-            failExceptions.addAll(Arrays.asList(classes));
-            return this;
-        }
+		public Builder retryWhenInstanceOf(Class<? extends Throwable>... classes) {
+			retryExceptions.addAll(Arrays.asList(classes));
+			return this;
+		}
 
-        public Builder retryIf(Func1<Throwable, Boolean> predicate) {
-            this.exceptionPredicate = predicate;
-            return this;
-        }
+		public Builder failWhenInstanceOf(Class<? extends Throwable>... classes) {
+			failExceptions.addAll(Arrays.asList(classes));
+			return this;
+		}
 
-        public Builder delays(Observable<Long> delays, TimeUnit unit) {
-            this.delays = delays.map(toMillis(unit));
-            return this;
-        }
+		public Builder retryIf(Func1<Throwable, Boolean> predicate) {
+			this.exceptionPredicate = predicate;
+			return this;
+		}
 
-        public Builder delay(Long delay, final TimeUnit unit) {
-            this.delays = Observable.just(delay).map(toMillis(unit)).repeat();
-            return this;
-        }
+		public Builder delays(Observable<Long> delays, TimeUnit unit) {
+			this.delays = delays.map(toMillis(unit));
+			return this;
+		}
+		
+		private static class ToLongHolder {
+			static final Func1<Integer, Long> INSTANCE = new Func1<Integer, Long>() {
+				@Override
+				public Long call(Integer n) {
+					if (n == null) {
+						return null;
+					} else {
+						return n.longValue();
+					}
+				}
+			};
+		}
+		
+		public Builder delaysInt(Observable<Integer> delays, TimeUnit unit) {
+			return delays(delays.map(ToLongHolder.INSTANCE), unit);
+		}
+		
+		public Builder delay(Long delay, final TimeUnit unit) {
+			this.delays = Observable.just(delay).map(toMillis(unit)).repeat();
+			return this;
+		}
 
-        private static Func1<Long, Long> toMillis(final TimeUnit unit) {
-            return new Func1<Long, Long>() {
+		private static Func1<Long, Long> toMillis(final TimeUnit unit) {
+			return new Func1<Long, Long>() {
 
-                @Override
-                public Long call(Long t) {
-                    return unit.toMillis(t);
-                }
-            };
-        }
+				@Override
+				public Long call(Long t) {
+					return unit.toMillis(t);
+				}
+			};
+		}
 
-        public Builder maxRetries(int maxRetries) {
-            this.maxRetries = of(maxRetries);
-            return this;
-        }
+		public Builder maxRetries(int maxRetries) {
+			this.maxRetries = of(maxRetries);
+			return this;
+		}
 
-        public Builder scheduler(Scheduler scheduler) {
-            this.scheduler = of(scheduler);
-            return this;
-        }
+		public Builder scheduler(Scheduler scheduler) {
+			this.scheduler = of(scheduler);
+			return this;
+		}
 
-        public Builder action(Action1<? super ErrorAndDuration> action) {
-            this.action = action;
-            return this;
-        }
+		public Builder action(Action1<? super ErrorAndDuration> action) {
+			this.action = action;
+			return this;
+		}
 
-        public Builder exponentialBackoff(final long firstDelay, final TimeUnit unit,
-                final double factor) {
-            delays = Observable.range(1, Integer.MAX_VALUE)
-                    // make exponential
-                    .map(new Func1<Integer, Long>() {
-                        @Override
-                        public Long call(Integer n) {
-                            return Math.round(Math.pow(factor, n - 1) * unit.toMillis(firstDelay));
-                        }
-                    });
-            return this;
-        }
+		public Builder exponentialBackoff(final long firstDelay, final TimeUnit unit, final double factor) {
+			delays = Observable.range(1, Integer.MAX_VALUE)
+					// make exponential
+					.map(new Func1<Integer, Long>() {
+						@Override
+						public Long call(Integer n) {
+							return Math.round(Math.pow(factor, n - 1) * unit.toMillis(firstDelay));
+						}
+					});
+			return this;
+		}
 
-        public Builder exponentialBackoff(long firstDelay, TimeUnit unit) {
-            return exponentialBackoff(firstDelay, unit, 2);
-        }
+		public Builder exponentialBackoff(long firstDelay, TimeUnit unit) {
+			return exponentialBackoff(firstDelay, unit, 2);
+		}
 
-        public Func1<Observable<? extends Throwable>, Observable<?>> build() {
-            Preconditions.checkNotNull(delays);
-            if (maxRetries.isPresent()) {
-                delays = delays.take(maxRetries.get());
-            }
-            return notificationHandler(delays, scheduler.get(), action, retryExceptions,
-                    failExceptions, exceptionPredicate);
-        }
+		public Func1<Observable<? extends Throwable>, Observable<?>> build() {
+			Preconditions.checkNotNull(delays);
+			if (maxRetries.isPresent()) {
+				delays = delays.take(maxRetries.get());
+			}
+			return notificationHandler(delays, scheduler.get(), action, retryExceptions, failExceptions,
+					exceptionPredicate);
+		}
 
-    }
+	}
 
-    public static final class ErrorAndDuration {
+	public static final class ErrorAndDuration {
 
-        private final Throwable throwable;
-        private final long durationMs;
+		private final Throwable throwable;
+		private final long durationMs;
 
-        public ErrorAndDuration(Throwable throwable, long durationMs) {
-            this.throwable = throwable;
-            this.durationMs = durationMs;
-        }
+		public ErrorAndDuration(Throwable throwable, long durationMs) {
+			this.throwable = throwable;
+			this.durationMs = durationMs;
+		}
 
-        public Throwable throwable() {
-            return throwable;
-        }
+		public Throwable throwable() {
+			return throwable;
+		}
 
-        public long durationMs() {
-            return durationMs;
-        }
+		public long durationMs() {
+			return durationMs;
+		}
 
-    }
+	}
 }
--- src/test/java/com/github/davidmoten/rx/RetryWhenTest.java
+++ src/test/java/com/github/davidmoten/rx/RetryWhenTest.java
@@ -1,4 +1,4 @@
-package com.github.davidmoten.rx.internal.operators;
+package com.github.davidmoten.rx;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
