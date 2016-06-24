5ae2acb4175f86e40990ce6a063fa8490ecf6beb
Dave Moten <davidmoten@gmail.com>
2016-06-24 13:34:53 +1000

    add perf for Strings.split

--- src/test/java/com/github/davidmoten/rx/Benchmarks.java
+++ src/test/java/com/github/davidmoten/rx/Benchmarks.java
@@ -1,5 +1,6 @@
 package com.github.davidmoten.rx;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -14,46 +15,59 @@ import rx.schedulers.Schedulers;
 
 public class Benchmarks {
 
-	@Benchmark
-	public void perfOnBackpressureBufferToFileFor100_000IntegersOnComputation(Blackhole bh) throws InterruptedException {
-		LatchedObserver<Integer> observer = new LatchedObserver<Integer>(bh);
-		Observable.range(1, 100000).compose(Transformers.onBackpressureBufferToFile(DataSerializers.integer()))
-				.subscribe(observer);
-		observer.latch.await(100, TimeUnit.SECONDS);
-	}
-	
-	@Benchmark
-	public void perfOnBackpressureBufferToFileFor100_000IntegersSychronous(Blackhole bh) throws InterruptedException {
-		LatchedObserver<Integer> observer = new LatchedObserver<Integer>(bh);
-		Observable.range(1, 100000).compose(Transformers.onBackpressureBufferToFile(DataSerializers.integer(),Schedulers.immediate()))
-				.subscribe(observer);
-		observer.latch.await(100, TimeUnit.SECONDS);
-	}
-	
-	@Benchmark
-	public void perfOnBackpressureBufferToFileFor3000_1KMessagesOnComputation(Blackhole bh) throws InterruptedException {
-		LatchedObserver<byte[]> observer = new LatchedObserver<byte[]>(bh);
-		Observable.range(1, 3000).map(new Func1<Integer, byte[]>() {
-			@Override
-			public byte[] call(Integer n) {
-				return new byte[1000];
-			}
-		}).compose(Transformers.onBackpressureBufferToFile(DataSerializers.byteArray(), Schedulers.immediate()))
-				.subscribe(observer);
-		observer.latch.await(100, TimeUnit.SECONDS);
-	}
-	
-	@Benchmark
-	public void perfOnBackpressureBufferToFileFor3000_1KMessagesOnSynchronous(Blackhole bh) throws InterruptedException {
-		LatchedObserver<byte[]> observer = new LatchedObserver<byte[]>(bh);
-		Observable.range(1, 3000).map(new Func1<Integer, byte[]>() {
-			@Override
-			public byte[] call(Integer n) {
-				return new byte[1000];
-			}
-		}).compose(Transformers.onBackpressureBufferToFile(DataSerializers.byteArray(), Schedulers.immediate()))
-				.subscribe(observer);
-		observer.latch.await(100, TimeUnit.SECONDS);
-	}
+    // @Benchmark
+    public void perfOnBackpressureBufferToFileFor100_000IntegersOnComputation(Blackhole bh)
+            throws InterruptedException {
+        LatchedObserver<Integer> observer = new LatchedObserver<Integer>(bh);
+        Observable.range(1, 100000)
+                .compose(Transformers.onBackpressureBufferToFile(DataSerializers.integer()))
+                .subscribe(observer);
+        observer.latch.await(100, TimeUnit.SECONDS);
+    }
+
+    // @Benchmark
+    public void perfOnBackpressureBufferToFileFor100_000IntegersSychronous(Blackhole bh)
+            throws InterruptedException {
+        LatchedObserver<Integer> observer = new LatchedObserver<Integer>(bh);
+        Observable.range(1, 100000).compose(Transformers
+                .onBackpressureBufferToFile(DataSerializers.integer(), Schedulers.immediate()))
+                .subscribe(observer);
+        observer.latch.await(100, TimeUnit.SECONDS);
+    }
+
+    // @Benchmark
+    public void perfOnBackpressureBufferToFileFor3000_1KMessagesOnComputation(Blackhole bh)
+            throws InterruptedException {
+        LatchedObserver<byte[]> observer = new LatchedObserver<byte[]>(bh);
+        Observable.range(1, 3000).map(new Func1<Integer, byte[]>() {
+            @Override
+            public byte[] call(Integer n) {
+                return new byte[1000];
+            }
+        }).compose(Transformers.onBackpressureBufferToFile(DataSerializers.byteArray(),
+                Schedulers.immediate())).subscribe(observer);
+        observer.latch.await(100, TimeUnit.SECONDS);
+    }
+
+    // @Benchmark
+    public void perfOnBackpressureBufferToFileFor3000_1KMessagesOnSynchronous(Blackhole bh)
+            throws InterruptedException {
+        LatchedObserver<byte[]> observer = new LatchedObserver<byte[]>(bh);
+        Observable.range(1, 3000).map(new Func1<Integer, byte[]>() {
+            @Override
+            public byte[] call(Integer n) {
+                return new byte[1000];
+            }
+        }).compose(Transformers.onBackpressureBufferToFile(DataSerializers.byteArray(),
+                Schedulers.immediate())).subscribe(observer);
+        observer.latch.await(100, TimeUnit.SECONDS);
+    }
+
+    @Benchmark
+    public void perfStringSplit() {
+        Observable.from(Arrays //
+                .asList("the quick brown ", "fox jumped over", " the lazy", " dog")) //
+                .compose(Transformers.split("o"));
+    }
 
 }
