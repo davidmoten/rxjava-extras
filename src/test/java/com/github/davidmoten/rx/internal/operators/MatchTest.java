506f46545d2a2cb3b6e4cc6ac9483bab4d95a6e1
Dave Moten <davidmoten@gmail.com>
2016-10-23 09:18:05 +1100

    match dev

--- src/test/java/com/github/davidmoten/rx/internal/operators/MatchTest.java
+++ src/test/java/com/github/davidmoten/rx/internal/operators/MatchTest.java
@@ -1,5 +1,11 @@
 package com.github.davidmoten.rx.internal.operators;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -20,9 +26,7 @@ public class MatchTest {
     public void test() {
         Observable<Integer> a = Observable.just(1, 2);
         Observable<Integer> b = Observable.just(2, 1);
-        match(a, b) //
-                .assertValues(Pair.create(2, 2), Pair.create(1, 1)) //
-                .assertCompleted();
+        match(a, b, 2, 1);
     }
 
     @SuppressWarnings("unchecked")
@@ -30,9 +34,15 @@ public class MatchTest {
     public void test2() {
         Observable<Integer> a = Observable.just(1, 2);
         Observable<Integer> b = Observable.just(1, 2);
-        match(a, b) //
-                .assertValues(Pair.create(1, 1), Pair.create(2, 2)) //
-                .assertCompleted();
+        match(a, b, 1, 2);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void test3() {
+        Observable<Integer> a = Observable.just(1, 2, 3);
+        Observable<Integer> b = Observable.just(3, 2, 1);
+        match(a, b, 3, 2, 1);
     }
 
     @SuppressWarnings("unchecked")
@@ -40,23 +50,28 @@ public class MatchTest {
     public void testOneMatch() {
         Observable<Integer> a = Observable.just(1);
         Observable<Integer> b = Observable.just(1);
-        match(a, b) //
-                .assertValues(Pair.create(1, 1)) //
-                .assertCompleted();
+        match(a, b, 1);
     }
-    
+
     @SuppressWarnings("unchecked")
     @Test
     @Ignore
     public void testRepeats() {
         Observable<Integer> a = Observable.just(1, 1);
         Observable<Integer> b = Observable.just(1, 1);
-        match(a, b) //
-                .assertValues(Pair.create(1, 1), Pair.create(1,1)) //
-                .assertCompleted();
+        match(a, b, 1, 1);
     }
 
-    private TestSubscriber2<Pair<Integer, Integer>> match(Observable<Integer> a,
+    private static void match(Observable<Integer> a, Observable<Integer> b, Integer... expected) {
+        List<Pair<Integer, Integer>> list = new ArrayList<Pair<Integer, Integer>>();
+        for (Integer i : expected) {
+            list.add(Pair.create(i, i));
+        }
+        TestSubscriber2<Pair<Integer, Integer>> ts = match(a, b).assertCompleted();
+        assertEquals(list, ts.getOnNextEvents());
+    }
+
+    private static TestSubscriber2<Pair<Integer, Integer>> match(Observable<Integer> a,
             Observable<Integer> b) {
         return a.compose(Transformers.matchWith(b, Functions.identity(), Functions.identity(),
                 new Func2<Integer, Integer, Pair<Integer, Integer>>() {
